50be1a385406a58955745acf72dc1a0c449fe3d5
ambermtse <atse@ucsd.edu>
2023-10-24 12:45:25 -0700

    Fixed bugs in ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void newTestReverseInPlace(){
+    int [] arr1 = {1, 4, 6};
+    ArrayExamples.reverseInPlace(arr1);
+    assertArrayEquals(new int[]{6, 4, 1}, arr1);
+  }
+
+  @Test
+  public void newTestReversed(){
+    int [] arr1 = {1, 4, 6};
+    assertArrayEquals(new int[]{6, 4, 1}, ArrayExamples.reversed(arr1));
+  }
+
 }
